d7dbfdbed62ae6fb0e2cfa7a3e34ae269ee6f768
perfect6566 <perfect6566@126.com>
2020-09-03 22:09:49 +0800

    Update check.go

--- goproject/check.go
+++ goproject/check.go
@@ -1,12 +1,19 @@
 package main
 
 import ("log"
+        "net/http"
 )
 
 
 func main(){
 
 log.Println("Welcome go")
+  
+http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("hello world!"))
+    })
+
+    http.ListenAndServe(":8080", nil)
 
 
 }
