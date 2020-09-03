5c9a7c911ba678212c2e6445ac33986fa757adbb
perfect6566 <perfect6566@126.com>
2020-09-03 21:21:03 +0800

    Update check.go

--- goproject/check.go
+++ goproject/check.go
@@ -1,22 +1,12 @@
 package main
 
 import ("log"
-"net/http")
+)
 
-func sayhelloGolang(w http.ResponseWriter, r *http.Request) {
-    r.ParseForm()  //解析参数，默认是不会解析的
-    log.Println("path", r.URL.Path)
-    w.Write([]byte("Hello Golang"))
-}
 
 func main(){
 
 log.Println("Welcome go")
-http.HandleFunc("/", sayhelloGolang) //设置访问的路由
-    err := http.ListenAndServe(":8080", nil) //设置监听的端口
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
 
 
 }
